180b45c9a5d4649a0ce50ab164418a8f4092cbda
Ajay Kumbhar <98207015+nobitanobi01@users.noreply.github.com>
2025-11-22 12:26:59 +0530

    Emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,10 +15,10 @@ const Contact = () => {
 
         emailjs
             .sendForm( //EmailJs serviceID, templateID, formRef, publicKey 
-                "service_jrvob9d",
-                "template_i8pwqm3",
+                " Enter your service Id",
+                "Enter your template Id",
                 form.current,
-                "brTOInCWBJlK4szm_"
+                "Enter your public key"
             )
             .then(
                 (result) => {
